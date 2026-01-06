dc4c71f73ec2a437ae5dfc41acb2b8060e4dc20e
ganeshbodakhe24 <156433243+ganeshbodakhe24@users.noreply.github.com>
2026-01-06 12:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-let inputs=document.querySelectorAll(".code");
-inputs.forEach((input,index)=>{
-	input.addEventListener("input",()=>{
-		input.value=input.value.slice(0,1);
-		if(input.value && index<inputs.length-1){
-			inputs[index+1].focus();
-		}
-	});
-	input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && !input.value && index > 0) {
-      inputs[index - 1].focus();
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+
+  input.addEventListener("input", () => {
+    // allow only one digit
+    input.value = input.value.slice(0, 1);
+
+    // move forward
+    if (input.value !== "" && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
-})
- 
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      }
+    }
+  });
+
+});
