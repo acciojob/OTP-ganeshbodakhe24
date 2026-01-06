97c936e8873dfbb4dde565738769f7a6aa256aeb
ganeshbodakhe24 <156433243+ganeshbodakhe24@users.noreply.github.com>
2026-01-06 12:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const inputs = document.querySelectorAll(".code");
 
+function setFocus(index) {
+  inputs.forEach(input => input.classList.remove("focused"));
+  inputs[index].classList.add("focused");
+  inputs[index].focus();
+}
+
 inputs.forEach((input, index) => {
 
   input.addEventListener("input", () => {
-    // allow only one digit
     input.value = input.value.slice(0, 1);
 
-    // move forward
-    if (input.value !== "" && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+    if (input.value && index < inputs.length - 1) {
+      setFocus(index + 1);
     }
   });
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-
       if (input.value === "" && index > 0) {
-        inputs[index - 1].focus();
         inputs[index - 1].value = "";
+        setFocus(index - 1);
       }
     }
   });
